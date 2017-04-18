3fb6f527cfa602b6100f883f57fbd2eb8c702992
KimSiHun <paradaise110@hanmail.net>
2017-04-18 10:39:57 +0900

    study recursion & iterator fac,fib

--- src/org/shkim/recursion/Recursion.java
+++ src/org/shkim/recursion/Recursion.java
@@ -2,35 +2,21 @@ package org.shkim.recursion;
 
 public class Recursion
 {
-	static int tester = 0;
 
 	// factorial recursion
-	public static long factorial(int N)
+	public static long recur_fac(int N)
 	{
 		if (N == 1)
 		{
 			return 1;
 		} else
 		{
-			return N * factorial(N - 1);
+			return N * recur_fac(N - 1);
 		}
-	}
-
-	// recursion
-	public static int fib(int N)
-	{
-		if (N == 0)
-		{
-			return 0;
-		} else if (N == 1)
-		{
-			return 1;
-		}
-		return (fib(N - 1) + fib(N - 2));
 	}
 
 	// iterate
-	public static long fac(int N)
+	public static long iter_fac(int N)
 	{
 		if (N == 0)
 		{
@@ -46,6 +32,48 @@ public class Recursion
 		}
 	}
 
+	// recursion
+	public static int recur_fib(int N)
+	{
+		if (N == 0)
+		{
+			return 0;
+		} else if (N == 1)
+		{
+			return 1;
+		}
+		return (recur_fib(N - 1) + recur_fib(N - 2));
+	}
+
+	// iterate
+	public static int iter_fib(int N)
+	{
+
+		if (N == 1)
+		{
+			return 1;
+		} else if (N == 2)
+		{
+			return 1;
+		}
+
+		int  t = 1, b = 1;
+		int temp = 1;
+		for (int i = 2; i < N; i++)
+		{
+			if (i == 2)
+			{
+				temp = 1 + b;
+			} else
+			{
+				t = temp;
+				temp = temp + b;
+				b = t;
+			}
+		}
+		return temp;
+	}
+
 	private static long timeDiffSec(long s, long e)
 	{
 		return (e - s);
@@ -56,8 +84,8 @@ public class Recursion
 	{
 		long s = System.currentTimeMillis();
 
-		System.out.println(fac(20));
-		
+		System.out.println(iter_fib(7));
+
 		long e = System.currentTimeMillis();
 
 		System.out.println(timeDiffSec(s, e) / 1000 + "sec");
